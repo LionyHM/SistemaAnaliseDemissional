5104b4db3c33bbe725e4e4705505834d1dad1fbc
“gat-liony.machado@es-gov” <liony.machado@prodest.es.gov.br>
2022-11-03 14:31:52 -0300

    feat: ajustado comportamento no filtro de busca do funcionário para demissão

--- src/components/Demissao/FormDemissaoComponent.tsx
+++ src/components/Demissao/FormDemissaoComponent.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ConteudoComponent from "../Layout/ConteudoComponent";
 
 export default function FormDemissaoComponent(){
@@ -6,15 +6,30 @@ export default function FormDemissaoComponent(){
         "Nome","Cpf","Data de Nascimento", "Setor", "Cargo", "Chefia", "Efetuar Demissão"
     ])
     const [exibirBusca,setExibirBusca] = useState("hidden")
-    const [nome, setNome] = useState("hidden")
+    const [nome, setNome] = useState("")
     const [resultadoBusca, setResultadoBusca] = useState([])
+    const inputElement = useRef(null);
+        useEffect(() => {
+            if (inputElement.current) {
+                inputElement.current.focus();
+            }
+        }, []);
+
+    useEffect(() => {
+        window.addEventListener("keydown", (e) => {
+            if(e.keyCode == 13){
+                renderBusca()
+            }
+        }
+         );
+    }, [nome])
 
     function renderCabecalho(){
         const arr = []
 
         for(let titulo = 0; titulo < titulosCabecalho.length; titulo++){
             arr.push(
-                <th className="w-1/6 min-w-[160px] text-sm font-semibold text-white border-l border-transparent">
+                <th key={titulo * Math.random()} className="w-1/6 min-w-[160px] text-sm font-semibold text-white border-l border-transparent">
                 {titulosCabecalho[titulo]}
                 </th>
             )
@@ -26,9 +41,9 @@ export default function FormDemissaoComponent(){
         const funcionario = await getFuncionario(nome)
         let arrFunc:any = []
 
-        funcionario.funcResponse.map(f => {
+        funcionario.funcResponse.map((f, i) => {
             arrFunc.push(
-                <tr>
+                <tr key={i * Math.random()}>
                     <td className="text-center text-dark font-medium text-sm bg-[#F3F6FF] border-b border-l border-[#E8E8E8]">
                         {f.Nome}
                     </td>
@@ -53,7 +68,7 @@ export default function FormDemissaoComponent(){
                     </td>
                     <td
                     className="text-center text-dark font-medium text-sm bg-white border-b border-r border-[#E8E8E8]">
-                    <a href="javascript:void(0)" className="border border-primary py-2 px-6 text-white inline-block rounded bg-red-400 hover:bg-red-800">
+                    <a href="#" className="border border-primary py-2 px-6 text-white inline-block rounded bg-red-400 hover:bg-red-800">
                         Confirmar
                     </a>
                     </td>
@@ -83,36 +98,36 @@ export default function FormDemissaoComponent(){
     return(
         <ConteudoComponent>
             <div className="w-full">
-                <div className="flex w-full flex-wrap absolute top-40">
+                <div className="flex w-full flex-wrap absolute top-36">
                     <div className="w-full px-3 mb-6 md:mb-0">
                         <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
                             Digite o nome do funcionário
                         </label>
-                        <input onChange={(e) => setNome(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-[#89a7b1] rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Digite o nome do funcionário" />
-                       <button onKeyDown={e => e.keyCode==13 ? console.log('Hello World from Input') : null} onClick={() => renderBusca()} className="btnSearchDefault text-white font-bold py-2 px-4 rounded">Buscar</button>
+                        <input ref={inputElement} onChange={(e) => setNome(e.target.value)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-[#89a7b1] rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Digite o nome do funcionário" />
+                       <button onClick={() => renderBusca()} className="btnSearchDefault text-white font-bold py-2 px-4 rounded">Buscar</button>
                     </div>
-                </div>
-                <div className={`p-10 ${exibirBusca}`}>
-                    <section className="bg-white py-20 lg:py-[120px]">
-                        <div className="container">
-                            <div className="flex flex-wrap -mx-4">
-                                <div className="w-full px-4">
-                                    <div className="max-w-full overflow-x-auto">
-                                    <table className="table-auto w-full">
-                                        <thead>
-                                            <tr className="text-center" style={{backgroundColor: "#566981"}}>
-                                               {renderCabecalho()}
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                           {resultadoBusca}                                          
-                                        </tbody>
-                                    </table>
+                    <div className={`${exibirBusca} w-full pl-10`}>
+                        <section className="bg-white py-20 lg:py-[120px]">
+                            <div className="container">
+                                <div className="flex flex-wrap -mx-4">
+                                    <div className="w-full px-4">
+                                        <div className="max-w-full overflow-x-auto">
+                                        <table className="table-auto w-full">
+                                            <thead>
+                                                <tr className="text-center" style={{backgroundColor: "#566981"}}>
+                                                {renderCabecalho()}
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                            {resultadoBusca}                                          
+                                            </tbody>
+                                        </table>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </section>
+                        </section>
+                    </div>
                 </div>
             </div>
 
